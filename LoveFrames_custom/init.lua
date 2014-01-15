3e82b597b8fd0b126820f160069ad1c79618925d
Leon Jiang <titangate1202@gmail.com>
2014-01-14 21:16:05 -0800

    Add a scene manager in loveFrames

---
+++ LoveFrames_custom/init.lua
@@ -0,0 +1,1 @@
+require 'LoveFrames_custom.scenerenderer'
---
+++ LoveFrames_custom/scenerenderer.lua
@@ -0,0 +1,120 @@
+--[[------------------------------------------------
+	-- Love Frames - A GUI library for LOVE --
+	-- Copyright (c) 2012-2014 Kenny Shields --
+--]]------------------------------------------------
+
+-- image object
+local newobject = loveframes.NewObject("scene_renderer", "loveframes_object_scene_renderer", true)
+
+--[[---------------------------------------------------------
+	- func: initialize()
+	- desc: initializes the object
+--]]---------------------------------------------------------
+function newobject:initialize()
+
+	self.type = "scene_renderer"
+	self.width = 0
+	self.height = 0
+	self.orientation = 0
+	self.scalex = 1
+	self.scaley = 1
+	self.offsetx = 0
+	self.offsety = 0
+	self.shearx = 0
+	self.sheary = 0
+	self.internal = false
+	
+end
+
+--[[---------------------------------------------------------
+	- func: update(deltatime)
+	- desc: updates the object
+--]]---------------------------------------------------------
+function newobject:update(dt)
+
+	local state = loveframes.state
+	local selfstate = self.state
+	
+	if state ~= selfstate then
+		return
+	end
+	
+	local visible = self.visible
+	local alwaysupdate = self.alwaysupdate
+	
+	if not visible then
+		if not alwaysupdate then
+			return
+		end
+	end
+	
+	local parent = self.parent
+	local base = loveframes.base
+	local update = self.Update
+	
+	self:CheckHover()
+	
+	-- move to parent if there is a parent
+	if parent ~= base then
+		self.x = self.parent.x + self.staticx
+		self.y = self.parent.y + self.staticy
+	end
+	
+	if update then
+		update(self, dt)
+	end
+	
+end
+
+--[[---------------------------------------------------------
+	- func: draw()
+	- desc: draws the object
+--]]---------------------------------------------------------
+function newobject:draw()
+	
+	local state = loveframes.state
+	local selfstate = self.state
+	
+	if state ~= selfstate then
+		return
+	end
+	
+	local visible = self.visible
+	
+	if not visible then
+		return
+	end
+	
+	local skins = loveframes.skins.available
+	local skinindex = loveframes.config["ACTIVESKIN"]
+	local defaultskin = loveframes.config["DEFAULTSKIN"]
+	local selfskin = self.skin
+	local skin = skins[selfskin] or skins[skinindex]
+	local drawfunc = skin.DrawImage or skins[defaultskin].DrawImage
+	local draw = self.Draw
+	local drawcount = loveframes.drawcount
+	
+	-- set the object's draw order
+	self:SetDrawOrder()
+		
+	if draw then
+		draw(self)
+	else
+		drawfunc(self)
+	end
+	
+end
+
+function newobject:Draw()
+	love.graphics.setColor(0,0,0,255)
+	love.graphics.rectangle('fill',self.x,self.y,self.width,self.height)
+	if not self.scene then
+		love.graphics.print("Scene not specified", self.x,self.y)
+		return 
+	end
+	self.scene:draw(self.x, self.y, self.width, self.height)
+end
+
+function newobject:SetScene(scene)
+	self.scene = scene
+end
